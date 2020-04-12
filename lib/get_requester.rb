f67cdc4cb591719a473519adbbc0af85deda9459
James Johnson <jtaylorjohnson@gmail.com>
2020-04-12 21:04:20 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -9,7 +9,7 @@ class GetRequester
     @url = url
   end
   
-  def get_respose_body
+  def get_response_body
     uri = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
     response.body
